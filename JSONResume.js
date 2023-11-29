a9b2b99e8fd7c1a6f33e4105134f224f7759f542
swornimamishra <112255377+swornimamishra@users.noreply.github.com>
2023-11-29 19:09:45 +0530

    Resume in JSON format uploaded

---
+++ JSONResume.js
@@ -0,0 +1,74 @@
+let a = {"Resume" : {
+    "name": "Swarnima mishra",
+    "picture": "",
+    "email": "swornimamishra@gmail.com",
+    "phone": "555-5555555",
+    "Degree": "B.E",
+    "website": "Your website URL",
+    "Address": {
+      "address": "kakori luckhnow u.p",
+      "postalCode": "226101",
+      "city": "luckhnow",
+    },
+    "profiles": [
+      {
+        "network": " LinkedIn",
+        "username": "kumari mishra swornima",
+        "url": "www.linkedin.com"
+      }
+    ]
+  },
+  "work": [
+    {
+      "company": "Google",
+      "position": "Full Stack Devloper",
+      "website": "www.google.com",
+      "startDate": "1-jan-2023",
+      "endDate": "Your end date, in YYY-MM-DD format (leave blank for a current position)",
+    }
+  ],
+  "education": [
+    {
+      "institution": "SSSUTMS",
+      "area": "sehore madhya pradesh",
+      "studyType": "graduation",
+      "startDate": "1-aug-2015",
+      "endDate": "15-6-2019",
+      "Cgpa": "7.6",
+      "courses": [
+        "B.E"
+      ]
+    }
+  ],
+  "awards": [
+    {
+      "title": "title of awards",
+      "date": " date, in YYYY-MM-DD format you received the award",
+      "awarder": " award given by",
+      "summary": "A one-sentence to one-paragraph overview of this award"
+    }
+  ],
+  "skills": [
+    {
+      "name": "html,css,js,Node js,mongoDb,react ,express js",
+      "keywords": [
+        "java script"
+      ]
+    }
+  ],
+  "languages": [
+    {
+      "language": "hindi,english,awadhi",
+      "fluency": "hindi,english,awadhi"
+    }
+  ],
+  "Hobbies": [
+    {
+      "name": "traveling,sports,reading book,exploring new place and learing new things ",
+      "keywords": [
+        "cricket,batminton"
+      ]
+    }
+  ]
+}
+
